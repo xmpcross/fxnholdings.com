ef70360d56816264b7f3d12b9dc97cdb853f6ddd
Vampy <kryptok112@gmail.com>
2026-06-21 18:30:34 +0800

    first

---
+++ README.md
@@ -0,0 +1,1 @@
+# fxnholdings.com
---
+++ contact-submit.php
@@ -0,0 +1,7 @@
+<?php
+$_SERVER['FXN_SITE_KEY'] = 'fxnholdings';
+$_SERVER['SITE_NAME'] = 'FXN Holdings';
+$_SERVER['RECIPIENT_EMAIL'] = 'info@fxnholdings.com';
+$_SERVER['SENDER_EMAIL'] = 'info@fxnholdings.com';
+$_SERVER['SENDER_NAME'] = 'FXN Holdings';
+require '/usr/local/share/fxn/brevo-contact-handler.php';
